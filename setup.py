82cc53841569abf56be0a62c16310b128005ab63
mishaschwartz <misha.schwartz@gmail.com>
2019-02-21 17:09:30 -0500

    authors: include Alessio Di Sandro as an author (should have been included originally)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fh:
 setuptools.setup(
     name="markusapi",
     version="0.0.1",
-    author="Misha Schwartz",
+    author="Alessio Di Sandro, Misha Schwartz",
     author_email="mschwa@cs.toronto.edu",
     description="Interface to interact with MarkUs API",
     long_description=long_description,
