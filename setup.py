06263d9b52c81c110874acb3ecc0babfdc9ebd79
mishaschwartz <misha.schwartz@gmail.com>
2019-10-16 10:23:51 -0400

    pip: finalize some requirements to make into a pip package

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Interface to interact with MarkUs API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/MarkUsProject/markus-api",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
