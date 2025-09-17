d957005f4073eb7d9ca78e63282b18913a30dc25
Donny Wong <donnywong@cs.toronto.edu>
2025-09-17 12:43:00 -0400

    reverting version as we are not doing a release yet

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="markusapi",
-    version="0.3.1",
+    version="0.3.0",
     author="Alessio Di Sandro, Misha Schwartz, Sam Maldonado",
     author_email="mschwa@cs.toronto.edu",
     description="Interface to interact with MarkUs API",
